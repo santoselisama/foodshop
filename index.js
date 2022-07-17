e564cdf927acb4814263432b26ffb2bd1c394541
Rhian Cavalcanti <rhiancavalcanti10@hotmail.com>
2022-07-17 20:21:14 -0300

    edit porta e node_modules

--- index.js
+++ index.js
@@ -152,6 +152,6 @@ app.get("/logout", (req, res) => {
 	res.redirect("/");
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
